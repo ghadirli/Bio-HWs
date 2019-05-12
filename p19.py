3be59a4079cfc56a89410cb5fa21cb5a066badf2
ghadirli <ghadirli@ce.sharif.edu>
2019-05-12 06:59:38 +0430

    p19 completed!

--- p19.py
+++ p19.py
@@ -1,95 +1,139 @@
+maximum_node = 0
+
+
 def main():
     matrix = []
     n = int(input())
+    D = {}
     for i in range(n):
         matrix.append([int(x) for x in input().split()])
-    neighbour_joining(matrix, n)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            D[(i, j)] = matrix[i][j]
+    max_node = n
+    edge, weight = neighbour_joining(D, n, max_node)
+    print(edge)
+    print(weight)
+    for i in sorted(edge):
+        for j in sorted(edge[i]):
+            print("%d->%d:%.3f" % (i, j, weight[(i, j)]))
 
 
-def neighbour_joining(matrix, n):
-    if n == 2:
-        tree = [[0, matrix[0][1]], [matrix[0][1], 0]]
-        return tree
-    nj_matrix = neighbour_joining_matrix(matrix)
-    i, j = find_non_diagonal_minimum(nj_matrix)
-    delta = (total_distance(matrix, i) - total_distance(matrix, j)) / (n - 2)
-    limb_length_i = (matrix[i][j] + delta) / 2
-    limb_length_j = (matrix[i][j] - delta) / 2
-    add_new_row_col(matrix, i, j)
-    remove_rows(matrix, i, j)
-    remove_cols(matrix, i, j)
-    tree = neighbour_joining(matrix, n - 1)
-    add_two_nodes(tree, limb_length_i, limb_length_j)
-    return tree
+def neighbour_joining(matrix, size, max_node):
+    if size == 2:
+        a = 0
+        b = 0
+        for key in matrix.keys():
+            if key[0] != key[1]:
+                a = key[0]
+                b = key[1]
+                break
+        edge = {a: [b], b: [a]}
+        weight = {(a, b): matrix[(a, b)], (b, a): matrix[(b, a)]}
+        return edge, weight
+    nj_matrix = neighbour_joining_matrix(matrix, size)
+    i, j = find_non_diagonal_minimum(nj_matrix, size)
+    print("i and j are", i, j)
+    delta = (total_distance(matrix, i) - total_distance(matrix, j)) / (size - 2)
+    limb_length_i = (matrix[(i, j)] + delta) / 2
+    limb_length_j = (matrix[(i, j)] - delta) / 2
+    add_new_row_col(matrix, i, j, max_node)
+    max_node += 1
+    size = size + 1
+    matrix = remove_rows(matrix, i, j)
+    matrix = remove_cols(matrix, i, j)
+    size = size - 2
+    edge, weight = neighbour_joining(matrix, size, max_node)
+
+    add_two_nodes(edge, weight, limb_length_i, limb_length_j, i, j, max_node - 1)
+    return edge, weight
 
 
-def add_two_nodes(tree, limb_length_i, limb_length_j):
-    tree.append([])
-    tree.append([])
-    for i in range(len(tree) - 2):
-        tree[i].append(0)
-        tree[i].append(0)
-    for i in range(len(tree)):
-        tree[len(tree) - 2].append(0)
-        tree[len(tree) - 1].append(0)
-    tree[len(tree) - 3][len(tree) - 2] = limb_length_i
-    tree[len(tree) - 3][len(tree) - 1] = limb_length_j
-    tree[len(tree) - 2][len(tree) - 3] = limb_length_i
-    tree[len(tree) - 1][len(tree) - 3] = limb_length_j
+def add_two_nodes(edge, weight, limb_length_i, limb_length_j, i, j, m):
+    edge[i] = [m]
+    edge[j] = [m]
+    edge[m].append(i)
+    edge[m].append(j)
+    weight[(i, m)] = weight[(m, i)] = limb_length_i
+    weight[(j, m)] = weight[(m, j)] = limb_length_j
 
 
 def remove_cols(matrix, i, j):
-    for k in range(len(matrix)):
-        del matrix[k][i]
-        del matrix[k][j]
+    temp = {}
+    for k in matrix.keys():
+        if k[1] != i:
+            temp[k] = matrix[k]
+    temp1 = {}
+    for k in temp.keys():
+        if k[1] != j:
+            temp1[k] = temp[k]
+    return temp1
 
 
 def remove_rows(matrix, i, j):
-    del matrix[i]
-    del matrix[j]
+    temp = {}
+    for k in matrix.keys():
+        if k[0] != i:
+            temp[k] = matrix[k]
+    temp1 = {}
+    for k in temp.keys():
+        if k[0] != j:
+            temp1[k] = temp[k]
+    return temp1
 
 
-def add_new_row_col(matrix, i, j):
-    matrix.append([])
-    for i in range(len(matrix)):
-        matrix.append(0)
-    m = len(matrix) - 1
-    for k in range(len(matrix)):
-        matrix[m][k] = matrix[k][m] = (matrix[k][i] + matrix[k][j] - matrix[i][j]) / 2
+def add_new_row_col(matrix, i, j, max_node):
+    matrix[(max_node, max_node)] = 0
+    s = set([i[0][0] for i in matrix.items()])
+    for k in s:
+        if k != max_node:
+            matrix[(k, max_node)] = (matrix[(k, i)] + matrix[(k, j)] - matrix[(i, j)]) / 2
+            matrix[(max_node, k)] = (matrix[(k, i)] + matrix[(k, j)] - matrix[(i, j)]) / 2
 
 
-def find_non_diagonal_minimum(matrix):
+def find_non_diagonal_minimum(matrix, n):
     minimum = float("inf")
-    min_index = (0, 0)
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if minimum > matrix[i][j] != 0:
-                minimum = matrix[i][j]
-                min_index = (i, j)
+    min_index = None
+    for i in matrix.items():
+        if minimum > matrix[i[0]] != 0:
+            minimum = matrix[i[0]]
+            min_index = i[0]
     return min_index
 
 
-def neighbour_joining_matrix(matrix):
-    n = len(matrix)
-    nj_matrix = initialize_with_zero(n)
-    for i in range(len(nj_matrix)):
-        for j in range(len(nj_matrix[0])):
-            if i != j:
-                nj_matrix[i][j] = (n - 2) * matrix[i][j] - total_distance(matrix, i) - total_distance(matrix, j)
+def neighbour_joining_matrix(matrix, size):
+    nj_matrix = initialize_with_zero(matrix)
+    for i in nj_matrix.items():
+        if i[0][0] != i[0][1]:
+            nj_matrix[i[0]] = (size - 2) * matrix[i[0]] - total_distance(matrix, i[0][0]) - total_distance(matrix,
+                                                                                                           i[0][1])
     return nj_matrix
 
 
 def total_distance(matrix, i):
-    return sum(matrix[i])
+    s = 0
+    for j in matrix.items():
+        if j[0][0] == i:
+            s += matrix[j[0]]
+    return s
 
 
-def initialize_with_zero(n):
-    matrix = []
+def initialize_with_zero(matrix):
+    my_matrix = {}
+    for i in matrix.items():
+        my_matrix[i[0]] = 0
+
+    return my_matrix
+
+
+def print_matrix(matrix, n):
     for i in range(n):
-        matrix.append([])
         for j in range(n):
-            matrix[i].append(0)
-    return matrix
+            if (i, j) in matrix:
+                print(matrix[(i, j)], end=" ")
+            else:
+                print(" ", end=" ")
+        print()
 
 
 if __name__ == '__main__':
